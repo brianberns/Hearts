b670ff4543a7a14e2e28dff87949fcc02f92cac0
Brian Berns <brianberns@gmail.com>
2020-12-18 21:04:29 -0500

    More work on basic gameplay.

--- Hearts/OpenDeal.fs
+++ Hearts/OpenDeal.fs
@@ -13,3 +13,37 @@ type OpenDeal =
         /// Each player's unplayed cards.
         UnplayedCardMap : Map<Seat, Set<Card>>
     }
+
+module OpenDeal =
+
+    /// Creates a deal from the given hands.
+    let fromHands dealer dir handMap =
+        assert(
+            let nCards =
+                handMap
+                    |> Map.toSeq
+                    |> Seq.collect snd
+                    |> Seq.distinct
+                    |> Seq.length
+            nCards = ClosedDeal.numCardsPerDeal)
+        {
+            Exchange = Exchange.create dir
+            ClosedDeal = ClosedDeal.create dealer
+            UnplayedCardMap = handMap
+        }
+
+    /// Deals cards from the given deck to each player.
+    let fromDeck dealer dir deck =
+        deck.Cards
+            |> Seq.indexed
+            |> Seq.groupBy (fun (iCard, _) ->
+                let n = (iCard + 1) % Seat.numSeats
+                dealer |> Seat.incr n)
+            |> Seq.map (fun (seat, group) ->
+                let cards =
+                    group
+                        |> Seq.map snd
+                        |> set
+                seat, cards)
+            |> Map
+            |> fromHands dealer dir
