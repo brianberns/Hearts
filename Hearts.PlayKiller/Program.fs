eda93cdf8d223be4b77a0c11470a07b2cdc24a6f
Brian Berns <brianberns@gmail.com>
2021-01-04 02:31:20 -0500

    Work on naive player.

--- Hearts.PlayKiller/Program.fs
+++ Hearts.PlayKiller/Program.fs
@@ -1,19 +1,48 @@
 ﻿namespace Hearts.PlayKiller
 
-open System
+open PlayingCards
 
 open Hearts
 
 module Naive =
 
-    let rng = Random(0)
-
     let makePass deal _ =
-        deal
-            |> OpenDeal.currentHand
-            |> Seq.sortByDescending (fun card -> card.Rank)
-            |> Seq.take Exchange.numCards
-            |> set
+
+        let hand = deal |> OpenDeal.currentHand
+
+            // determine spades to pass
+        let spadesToPass =
+            let spadeRanks =
+                hand
+                    |> Seq.where (fun card ->
+                        card.Suit = Suit.Spades)
+                    |> Seq.map (fun card -> card.Rank)
+                    |> set
+            let nSpades = spadeRanks.Count
+            let toPass =
+                if nSpades > 4
+                    || (nSpades = 4
+                        && spadeRanks.Contains(Rank.Queen)) then
+                    fun _ -> false
+                else
+                    fun rank -> rank >= Rank.Queen
+            spadeRanks
+                |> Seq.where toPass
+                |> Seq.map (fun rank -> Card(rank, Suit.Spades))
+                |> set
+                
+            // determine other cards to pass
+        let othersToPass =
+            let nOthers =
+                Exchange.numCards - spadesToPass.Count
+            hand
+                |> Seq.where (fun card ->
+                    card.Suit <> Suit.Spades)
+                |> Seq.sortDescending
+                |> Seq.take nOthers
+                |> Set
+
+        spadesToPass + othersToPass
 
     let makePlay deal _ =
         let hand = OpenDeal.currentHand deal
