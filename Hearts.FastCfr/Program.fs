7df760d953b6621e8648e8002096a1a3b7f63092
Brian Berns <brianberns@gmail.com>
2024-10-25 14:25:26 -0400

    Improved info set key generation.

--- Hearts.FastCfr/Program.fs
+++ Hearts.FastCfr/Program.fs
@@ -7,14 +7,49 @@ open FastCfr
 
 module Program =
 
-    let getInfoSetKey hand deal =
-        [|
-            Hand.toString hand
-            for trick in ClosedDeal.tricks deal do
-                sprintf " %c" trick.Leader.Char
-                for card in Seq.rev trick.Cards do
-                    sprintf " %A" card
-        |] |> String.concat ""
+    let getInfoSetKey (hand : Hand) deal =
+        let sb = Text.StringBuilder()
+
+            // hand and other unplayed cards
+        for card in Card.allCards do
+            sb.Append(
+                if hand.Contains(card) then
+                    assert(deal.UnplayedCards.Contains(card))
+                    'o'
+                elif deal.UnplayedCards.Contains(card) then 'x'
+                else '.')
+                |> ignore
+
+            // current trick
+        let trick = ClosedDeal.currentTrick deal
+        sb.Append(trick.Leader.Char) |> ignore
+        let cards =
+            trick.Cards
+                |> List.rev
+                |> List.toArray
+        for iCard = 0 to Seat.numSeats - 1 do
+            sb.Append(
+                if iCard < cards.Length then
+                    cards[iCard].String
+                else "..")
+                |> ignore
+
+            // voids
+        let player = Trick.currentPlayer trick
+        for seat in Enum.getValues<Seat> do
+            if seat <> player then
+                for suit in Enum.getValues<Suit> do
+                    sb.Append(
+                        if deal.Voids.Contains(seat, suit) then 'x'
+                        else '.')
+                        |> ignore
+
+            // score
+        for score in deal.Score.ScoreMap.Values do
+            assert(score >= 0 && score < 10)
+            sb.Append(score) |> ignore
+
+        sb.ToString()
 
     let canTryFinalize deal =
         deal.ClosedDeal.CurrentTrickOpt
@@ -29,7 +64,14 @@ module Program =
             else None
         match scoreOpt with
             | Some score ->
-                let payoff = float score[Seat.South]
+                let otherAvg =
+                    (score.ScoreMap
+                        |> Map.toSeq
+                        |> Seq.where (fun (seat, _) -> seat <> Seat.South)
+                        |> Seq.sumBy snd
+                        |> float)
+                        / float (Seat.numSeats - 1)
+                let payoff = otherAvg - float score[Seat.South]
                 TerminalGameState.create 0 payoff
                     |> Terminal
             | None ->
@@ -63,22 +105,34 @@ module Program =
                     |> OpenDeal.startPlay
                     |> createGameState)
                 |> Seq.chunkBySize chunkSize
+                |> Seq.mapi (fun iChunk chunk ->
+                    printfn $"Chunk {iChunk}"
+                    chunk)
         Trainer.train (rng.Next()) gameChunks
 
     let run () =
 
             // train
-        let numGames = 100
-        let chunkSize = 2
+        let numGames = 10000
+        let chunkSize = 1000
         let util, infoSetMap = train numGames chunkSize
 
             // expected overall utility
         printfn $"Average game value for first player: %0.5f{util}\n"
+        printfn $"# of info sets: {infoSetMap.Count}"
 
         printfn ""
-        for (KeyValue(key, infoSet)) in infoSetMap do
-            let strategy =
-                InformationSet.getAverageStrategy infoSet
+        let pairs =
+            infoSetMap
+                |> Map.toSeq
+                |> Seq.map (fun (key, infoSet) ->
+                    key,
+                    InformationSet.getAverageStrategy infoSet)
+                |> Seq.where (fun (_, strategy) ->
+                    strategy
+                        |> Seq.distinct
+                        |> Seq.length > 1)
+        for (key, strategy) in pairs do
             printfn $"{key}: %A{strategy.ToArray()}"
 
     Console.OutputEncoding <- Text.Encoding.UTF8
