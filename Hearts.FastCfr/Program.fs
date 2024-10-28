cb764c921f02780d523f98982d87ee1486c512bd
Brian Berns <brianberns@gmail.com>
2024-10-28 07:38:50 -0400

    More memory savings.

--- Hearts.FastCfr/Program.fs
+++ Hearts.FastCfr/Program.fs
@@ -11,49 +11,66 @@ open MathNet.Numerics.LinearAlgebra
 module Program =
 
     let getInfoSetKey (hand : Hand) deal =
-        let bytes = ResizeArray<byte>(41)
+        let key = ResizeArray<uint16>(12)
 
-            // hand and other unplayed cards
-        for card in Card.allCards do
-            bytes.Add(
+            // hand
+        assert(Card.allCards.Length <= 16)
+        Seq.sum [|
+            for i, card in Seq.indexed Card.allCards do
                 if hand.Contains(card) then
-                    assert(deal.UnplayedCards.Contains(card))
-                    2uy
-                elif deal.UnplayedCards.Contains(card) then 1uy
-                else 0uy)
+                    let value = 1us <<< i
+                    assert(value >>> i = 1us)
+                    value
+        |] |> key.Add
+
+            // other unplayed cards
+        Seq.sum [|
+            for i, card in Seq.indexed Card.allCards do
+                if not (hand.Contains(card))
+                    && deal.UnplayedCards.Contains(card) then
+                    let value = 1us <<< i
+                    assert(value >>> i = 1us)
+                    value
+        |] |> key.Add
 
             // current trick
         let trick = ClosedDeal.currentTrick deal
-        bytes.Add(byte trick.Leader)
+        key.Add(uint16 trick.Leader)
         let cards =
             trick.Cards
                 |> List.rev
                 |> List.toArray
         for iCard = 0 to Seat.numSeats - 1 do
-            bytes.Add(
+            key.Add(
                 if iCard < cards.Length then
-                    byte cards[iCard].Rank
-                else 0uy)
-            bytes.Add(
-                if iCard < cards.Length then
-                    byte cards[iCard].Suit
-                else 0uy)
+                    let value =
+                        (uint16 cards[iCard].Rank <<< 8) +
+                            uint16 cards[iCard].Suit
+                    assert((value / (1us <<< 8)) = uint16 cards[iCard].Rank)
+                    assert((value % (1us <<< 8)) = uint16 cards[iCard].Suit)
+                    value
+                else 0us)
 
             // voids
         let player = Trick.currentPlayer trick
-        for seat in Enum.getValues<Seat> do
-            if seat <> player then
-                for suit in Enum.getValues<Suit> do
-                    bytes.Add(
-                        if deal.Voids.Contains(seat, suit) then 1uy
-                        else 0uy)
+        let pairs =
+            [|
+                for seat in Enum.getValues<Seat> do
+                    if seat <> player then
+                        for suit in Enum.getValues<Suit> do
+                            yield seat, suit
+            |]
+        Seq.sum [|
+            for i, pair in Seq.indexed pairs do
+                if deal.Voids.Contains(pair) then
+                    1us <<< i
+        |] |> key.Add
 
             // score
         for score in deal.Score.ScoreMap.Values do
-            assert(score >= 0 && score < 10)
-            bytes.Add(byte score)
+            key.Add(uint16 score)
 
-        bytes.ToArray()
+        key.ToArray()
 
     let canTryFinalize deal =
         deal.ClosedDeal.CurrentTrickOpt
@@ -73,9 +90,9 @@ module Program =
                         |> Map.toSeq
                         |> Seq.where (fun (seat, _) -> seat <> Seat.South)
                         |> Seq.sumBy snd
-                        |> float)
-                        / float (Seat.numSeats - 1)
-                let payoff = otherAvg - float score[Seat.South]
+                        |> float32)
+                        / float32 (Seat.numSeats - 1)
+                let payoff = otherAvg - float32 score[Seat.South]
                 TerminalGameState.create 0 payoff
                     |> Terminal
             | None ->
@@ -114,14 +131,15 @@ module Program =
                     chunk)
         Trainer.train (rng.Next()) gameChunks
 
-    let save (strategyMap : Map<byte[], Vector<float>>) =
+    let save (strategyMap : Map<uint16[], Vector<float32>>) =
         let path = "Hearts.strategy"
         use stream = new FileStream(path, FileMode.Create)
         use wtr = new BinaryWriter(stream)
         wtr.Write(strategyMap.Count)
         for (KeyValue(key, strategy)) in strategyMap do
-            assert(key.Length = 41)
-            wtr.Write(key)
+            assert(key.Length = 12)
+            for value in key do
+                wtr.Write(value)
             wtr.Write(uint8 strategy.Count)
             for prob in strategy do
                 wtr.Write(prob)
@@ -129,8 +147,8 @@ module Program =
     let run () =
 
             // train
-        let numGames = 100000
-        let chunkSize = 1000
+        let numGames = 10_000_000
+        let chunkSize = 100_000
         let util, infoSetMap = train numGames chunkSize
 
             // expected overall utility
