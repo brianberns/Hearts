e301b2bbdf580fe07c35b3c5ad038871a19232ff
Brian Berns <brianberns@gmail.com>
2024-10-31 09:12:35 -0400

    Modularized.

---
+++ Hearts.Web/Server/Remoting.fs
@@ -0,0 +1,43 @@
+﻿namespace Hearts.Web
+
+open System
+open System.IO
+
+open Hearts.FastCfr
+
+open MathNet.Numerics.Distributions
+
+open Fable.Remoting.Server
+open Fable.Remoting.Suave
+
+module Remoting =
+
+    /// Hearts API.
+    let private heartsApi dir =
+
+        let strategyMap =
+            let path = Path.Combine(dir, "Hearts.strategy")
+            Strategy.load path
+
+        let lookup key =
+            Map.tryFind key strategyMap
+
+        let rng = Random(0)
+
+        {
+            GetPlayIndex =
+                fun key ->
+                    async {
+                        return lookup key
+                            |> Option.map (fun strategy ->
+                                Categorical.Sample(rng, strategy))
+                    }
+            GetStrategy =
+                fun key -> async { return lookup key }
+        }
+
+    /// Build API.
+    let webPart dir =
+        Remoting.createApi()
+            |> Remoting.fromValue (heartsApi dir)
+            |> Remoting.buildWebPart
--- Hearts.Web/Server/WebPart.fs
+++ Hearts.Web/Server/WebPart.fs
@@ -1,44 +1,5 @@
 ﻿namespace Hearts.Web
 
-module Remoting =
-
-    open System
-    open System.IO
-    open Hearts.FastCfr
-    open MathNet.Numerics.Distributions
-
-    let private heartsApi dir =
-
-        let strategyMap =
-            let path = Path.Combine(dir, "Hearts.strategy")
-            Strategy.load path
-
-        let lookup key =
-            Map.tryFind key strategyMap
-
-        let rng = Random(0)
-
-        {
-            GetPlayIndex =
-                fun key ->
-                    async {
-                        return lookup key
-                            |> Option.map (fun strategy ->
-                                Categorical.Sample(rng, strategy))
-                    }
-            GetStrategy =
-                fun key -> async { return lookup key }
-        }
-
-    open Fable.Remoting.Server
-    open Fable.Remoting.Suave
-
-    /// Build API.
-    let webPart dir =
-        Remoting.createApi()
-            |> Remoting.fromValue (heartsApi dir)
-            |> Remoting.buildWebPart
-
 module WebPart =
 
     open System.IO
