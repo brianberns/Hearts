b13e2f80f00daa50f692f15062d9fdff865de958
Brian Berns <brianberns@gmail.com>
2024-12-02 17:05:00 -0500

    Switch to named type.

--- Hearts.DeepCfr/Settings.fs
+++ Hearts.DeepCfr/Settings.fs
@@ -2,6 +2,46 @@
 
 open System
 
+/// Hyperparameters.
+type Settings =
+    {
+        /// Random number generator.
+        Random : Random
+
+        /// Compensation for n-1 players on the other team.
+        CutthroatCompensation : int
+
+        /// Size of a neural network hidden layer.
+        HiddenSize : int
+
+        /// Optimizer learning rate.
+        LearningRate : float
+
+        /// Number of steps to use when training advantage models.
+        NumAdvantageTrainSteps : int
+
+        /// Number of advantage samples to keep.
+        NumAdvantageSamples : int
+
+        /// Number of deals to traverse during each iteration.
+        NumTraversals : int
+
+        /// Number of iterations to perform.
+        NumIterations : int
+
+        /// Number of steps to use when training the strategy model.
+        NumStrategyTrainSteps : int
+
+        /// Number of strategy samples to keep.
+        NumStrategySamples : int
+
+        /// Tensorboard writer.
+        Writer : TorchSharp.Modules.SummaryWriter
+
+        /// Verbose output?
+        Verbose : bool
+    }
+
 [<AutoOpen>]
 module Settings =
 
@@ -9,40 +49,20 @@ module Settings =
     let settings =
         let seed = 0
         TorchSharp.torch.manual_seed(seed) |> ignore
-        {|
-            /// Random number generator.
+        {
             Random = Random(seed)
-
-            /// Size of a neural network hidden layer.
+            CutthroatCompensation = 9
             HiddenSize = Encoding.encodedLength * 2
-
-            /// Optimizer learning rate.
             LearningRate = 1e-3
-
-            /// Number of steps to use when training advantage models.
             NumAdvantageTrainSteps = 2000
-
-            /// Number of advantage samples to keep.
             NumAdvantageSamples = 1_000_000
-
-            /// Number of deals to traverse during each iteration.
             NumTraversals = 10
-
-            /// Number of iterations to perform.
             NumIterations = 400
-
-            /// Number of steps to use when training the strategy model.
             NumStrategyTrainSteps = 4000
-
-            /// Number of strategy samples to keep.
             NumStrategySamples = 1_000_000
-
-            /// Tensorboard writer.
             Writer =
                 let timespan = DateTime.Now - DateTime.Today
                 TorchSharp.torch.utils.tensorboard.SummaryWriter(
                     $"runs/run%05d{int timespan.TotalSeconds}")
-
-            /// Verbose output?
             Verbose = true
-        |}
+        }
