3b4a9b5970077e402a72f55a00fd41e1d2bfa4b2
Brian Berns <brianberns@gmail.com>
2025-02-07 15:10:57 -0500

    Split Hearts.Model from Hearts.Learn.

--- Hearts.Learn/Traverse.fs
+++ Hearts.Learn/Traverse.fs
@@ -2,7 +2,6 @@
 
 open MathNet.Numerics.LinearAlgebra
 
-open PlayingCards
 open Hearts
 
 /// Model Hearts as a zero-sum game.
@@ -30,51 +29,6 @@ module ZeroSum =
     let tryGetPayoff deal =
         Game.tryUpdateScore deal Score.zero
             |> Option.map getPayoff
-
-module Strategy =
-
-    /// Computes strategy from the given per-action regrets.
-    /// A strategy is normalized so that its elements sum
-    /// to 1.0 (to represent action probabilities).
-    let private matchRegrets regrets =
-
-            // find highest-value action
-        let idx = Vector.maxIndex regrets
-
-            // scale if possible, or choose highest-value action
-        if regrets[idx] > 0.0f then
-            let clamped = Vector.map (max 0.0f) regrets   // clamp negative regrets
-            clamped / Vector.sum clamped
-        else
-            DenseVector.init regrets.Count (fun i ->
-                if i = idx then 1.0f
-                else 0.0f)
-
-    /// Converts a wide vector (indexed by entire deck) to
-    /// a narrow vector (indexed by legal plays).
-    let toNarrow (legalPlays : _[]) (wide : Vector<_>) =
-        assert(wide.Count = Card.numCards)
-        legalPlays
-            |> Seq.map (
-                Card.toIndex >> Vector.get wide)
-            |> DenseVector.ofSeq
-
-    /// Converts a narrow vector (indexed by legal plays) to
-    /// a wide vector (indexed by entire deck).
-    let toWide (legalPlays : _[]) (narrow : Vector<_>) =
-        assert(narrow.Count = legalPlays.Length)
-        Seq.zip legalPlays narrow
-            |> Encoding.encodeCardValues
-            |> DenseVector.ofArray
-
-    /// Computes strategy for the given info set (hand + deal)
-    /// using the given advantage model.
-    let getFromAdvantage (model : AdvantageModel) hand deal legalPlays =
-        (AdvantageModel.getAdvantage hand deal model)
-            .data<float32>()
-            |> DenseVector.ofSeq
-            |> toNarrow legalPlays
-            |> matchRegrets
 
 module Traverse =
 
