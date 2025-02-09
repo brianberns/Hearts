3235103115b257f9121773bec6ca770ed4d52c38
Brian Berns <brianberns@gmail.com>
2025-02-09 16:22:33 -0500

    Switch back to mini Hearts, but slightly larger than before.

--- PlayingCards/Rank.fs
+++ PlayingCards/Rank.fs
@@ -2,12 +2,14 @@
 
 /// The rank of a card.
 type Rank =
+#if !MINI
     | Two   =  2
     | Three =  3
     | Four  =  4
     | Five  =  5
     | Six   =  6
     | Seven =  7
+#endif
     | Eight =  8
     | Nine  =  9
     | Ten   = 10
