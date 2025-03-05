6a01672b318343a7ddc1bc605c4b0d555eb9ccd7
Brian Berns <brianberns@gmail.com>
2025-03-05 17:26:24 -0500

    Commented.

--- Hearts/Exchange.fs
+++ Hearts/Exchange.fs
@@ -10,12 +10,15 @@ module Pass =
     /// Number of cards passed by each player.
     let numCards = 3
 
+    /// Empty pass to which cards will be added.
     let empty : Pass = Set.empty
 
+    /// Is the given pass ready to be delivered?
     let isComplete (pass : Pass) =
         assert(pass.Count <= numCards)
         pass.Count = numCards
 
+    /// Adds the given card to the given pass.
     let add card (pass : Pass) : Pass =
         assert(pass.Count < numCards)
         assert(pass.Contains(card) |> not)
