f9bf928377f6f6594b216d8114cfef370f69cbd7
Brian Berns <brianberns@gmail.com>
2025-04-16 09:52:12 -0400

    Restored full encoding details.

--- Hearts.Model/Encoding.fs
+++ Hearts.Model/Encoding.fs
@@ -60,15 +60,15 @@ module Encoding =
                 cardIndexes.Contains(index)
         |]
 
-    module Exchange =
+    /// Encodes the given exchange direction as a one-hot
+    /// vector in the number of exchange directions.
+    let private encodeExchangeDirection dir =
+        [|
+            for d in Enum.getValues<ExchangeDirection> do
+                d = dir
+        |]
 
-        /// Encodes the given exchange direction as a one-hot
-        /// vector in the number of exchange directions.
-        let private encodeDirection dir =
-            [|
-                for d in Enum.getValues<ExchangeDirection> do
-                    d = dir
-            |]
+    module Exchange =
  
         /// Total encoded length of an exchange info set.
         let encodedLength =
@@ -81,7 +81,7 @@ module Encoding =
             let encoded =
                 BitArray [|
                     yield! encodeCards infoSet.Hand      // current player's unpassed cards
-                    yield! encodeDirection               // exchange direction
+                    yield! encodeExchangeDirection       // exchange direction
                         infoSet.Deal.ExchangeDirection
                     match infoSet.OutgoingPassOpt with   // outgoing pass, so far
                         | Some pass ->
@@ -95,6 +95,15 @@ module Encoding =
             encoded
 
     module Playout =
+
+        /// Encodes the given pass as a multi-hot vector in
+        /// the deck size.
+        let private encodePass passOpt =
+            let cards : Pass =
+                passOpt
+                    |> Option.defaultValue Set.empty
+            assert(cards.Count <= Pass.numCards)
+            encodeCards cards
 
         /// Encodes each card in the given current trick as
         /// a one-hot vector in the deck size and concatenates
@@ -118,7 +127,6 @@ module Encoding =
                         |> encodeCards
             |]
 
-        (*
         /// Encodes the given voids as a multi-hot vector in the
         /// number of suits times the number of other seats.
         let private encodeVoids player voids =
@@ -127,11 +135,8 @@ module Encoding =
                     let seats =
                         Seat.cycle player |> Seq.skip 1
                     for seat in seats do
-                        if Set.contains (seat, suit) voids then
-                            1uy
-                        else 0uy
+                        Set.contains (seat, suit) voids
             |]
-        *)
 
         /// Encodes the given score as a multi-hot vector in the
         /// number of seats.
@@ -146,15 +151,11 @@ module Encoding =
         let encodedLength =
             Card.numCards                                 // current player's hand
                 + Card.numCards                           // unplayed cards not in current player's hand
-                (*
                 + ExchangeDirection.numDirections         // exchange direction
                 + Card.numCards                           // outgoing pass
                 + Card.numCards                           // incoming pass
-                *)
                 + ((Seat.numSeats - 1) * Card.numCards)   // current trick
-                (*
                 + ((Seat.numSeats - 1) * Suit.numSuits)   // voids
-                *)
                 + Seat.numSeats                           // score
 
         /// Encodes the given playout info set as a vector.
@@ -166,17 +167,13 @@ module Encoding =
                 BitArray [|
                     yield! encodeCards infoSet.Hand             // current player's hand
                     yield! encodeCards unseen                   // unplayed cards not in current player's hand
-                    (*
                     yield! encodeExchangeDirection              // exchange direction
                         infoSet.Deal.ExchangeDirection
                     yield! encodePass infoSet.OutgoingPassOpt   // outgoing pass
                     yield! encodePass infoSet.IncomingPassOpt   // incoming pass
-                    *)
                     yield! encodeTrick trickOpt                 // current trick
-                    (*
                     yield! encodeVoids                          // voids
                         infoSet.Player infoSet.Deal.Voids
-                    *)
                     yield! encodeScore                          // score
                         infoSet.Player infoSet.Deal.Score
                 |]
