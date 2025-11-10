31310c005d551effd1a5cbf534c4aaec0f4c4bf1
Brian Berns <brianberns@gmail.com>
2025-11-10 14:27:45 -0500

    More work on tree search.

--- Hearts.Tree/Program.fs
+++ Hearts.Tree/Program.fs
@@ -1,7 +1,22 @@
 ﻿namespace Hearts.Tree
 
+open System
+open Hearts
+
 module Program =
 
-    let act infoSet =
-        ()
+    let act (infoSet : InformationSet) =
+        infoSet.LegalActions[0]
 
+    let player = { Act = act }
+
+    let score, payoff =
+        Tournament.run
+            (Random(0))
+            1000
+            player
+            player
+
+    for (KeyValue(seat, points)) in score.ScoreMap do
+        printfn $"%-6s{string seat}: {points}"
+    printfn $"Payoff: %0.5f{payoff}"
