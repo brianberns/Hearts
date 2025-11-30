ecdc22d90249e5011e0d524c1b9d25ba96da7d52
Brian Berns <brianberns@gmail.com>
2025-11-30 13:40:21 -0500

    Full round trip.

--- Hearts.ParseKiller/Program.fs
+++ Hearts.ParseKiller/Program.fs
@@ -4,15 +4,9 @@ open System
 open System.Text
 
 module Program =
-
-    let write () =
-        Log.convertToJson
-            @"C:\Users\brian\OneDrive\Desktop\KHearts.log"
-            "KHearts.json"
-
-    let read () =
-        Json.loadEntries "KHearts.json"
-            |> Json.saveEntries "KHearts.copy.json"
-
     Console.OutputEncoding <- Encoding.Unicode
-    read ()
+    Log.convertToJson
+        @"C:\Users\brian\OneDrive\Desktop\KHearts.log"
+        "KHearts.json"
+    Json.loadEntries "KHearts.json"
+        |> Json.saveEntries "KHearts.copy.json"
