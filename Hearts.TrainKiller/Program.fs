b82ada7bdbb5fd0fdf673b2a08de1e1092c9c9db
Brian Berns <brianberns@gmail.com>
2025-12-01 18:22:22 -0500

    Began work on training KHearts model.

---
+++ Hearts.TrainKiller/Program.fs
@@ -0,0 +1,15 @@
+﻿namespace Hearts.TrainKiller
+
+open System
+open System.Text
+
+open Hearts
+open Hearts.ParseKiller
+
+module Program =
+    Console.OutputEncoding <- Encoding.Unicode
+    let entries =
+        Json.loadEntries @"C:\Users\brian\OneDrive\Desktop\KHearts.json"
+            |> Array.where (fun entry ->
+                entry.GameScore = Score.zero)
+    printfn "%A entries" entries.Length
