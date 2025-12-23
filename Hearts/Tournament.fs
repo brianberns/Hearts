ed2378d226f631ad71b5412fa72d13364e3ded57
Brian Berns <brianberns@gmail.com>
2025-12-23 12:06:25 -0500

    More work on game-oriented model.

--- Hearts/Tournament.fs
+++ Hearts/Tournament.fs
@@ -20,14 +20,25 @@ module Tournament =
                 |> Map
 
             // run games
-        let score =
+        let games =
             Game.playGames rng inParallel numGames (
                 Game.playGame rng playerMap)
-                |> Seq.map _.Score
-                |> Seq.reduce (+)
 
-            // compute payoff for challenger
+            // compute results
+        let winnerMap =
+            games
+                |> Seq.collect (Game.findGameWinners)
+                |> Seq.groupBy id
+                |> Seq.map (fun (seat, group) ->
+                    seat, Seq.length group)
+                |> Map
+        let score =
+            Score.ofPoints [|
+                for seat in Enum.getValues<Seat> do
+                    match Map.tryFind seat winnerMap with
+                        | Some wins -> wins
+                        | None -> 0
+            |]
         let payoff =
-            (Game.getPayoffs score)[int challengerSeat]
-                / float32 numGames
+            (float32 score[challengerSeat] / float32 numGames)
         score, payoff
