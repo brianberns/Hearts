61457caa317af6611bc5c3d39451ee2d50803e47
Brian Berns <brianberns@gmail.com>
2026-01-06 13:08:04 -0500

    Hyperparameter search.

--- Hearts.Learn/Program.fs
+++ Hearts.Learn/Program.fs
@@ -25,7 +25,7 @@ module Program =
                             { Settings.create writer with
                                 NumDealsPerIteration = 4000
                                 SampleBranchRate = 0.17
-                                SampleReservoirCapacity = 1_000_000
+                                SampleReservoirCapacity = 2_000_000
                                 NumIterations = 4
                                 NumTrainingEpochs = 500
                                 NumHiddenLayers = numHiddenLayers
