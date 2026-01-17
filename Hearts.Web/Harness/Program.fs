19407d309a57b831825f9b86ef778550b533619b
Brian Berns <brianberns@gmail.com>
2026-01-16 23:36:12 -0500

    Updated to Suave v3.

--- Hearts.Web/Harness/Program.fs
+++ Hearts.Web/Harness/Program.fs
@@ -1,9 +1,5 @@
 ﻿open System.Net
-
 open Suave
-open Suave.Filters
-open Suave.Logging
-open Suave.Operators
 
 let config =
     {
@@ -13,13 +9,9 @@ let config =
     }
 
 let app =
-    let logger = Targets.create LogLevel.Info [||]
     choose [
         Dynamic.WebPart.fromToml "WebParts.toml"
         RequestErrors.NOT_FOUND "Found no handlers."
-    ] >=> logWithLevelStructured
-        LogLevel.Info
-        logger
-        logFormatStructured
+    ]
 
 startWebServer config app
