23a70230c097bd0bf49c6b5ee8ebf7148c50b277
Brian Berns <brianberns@gmail.com>
2026-01-16 23:50:42 -0500

    Updated packages.

--- Hearts.Web/Client/webpack.config.js
+++ Hearts.Web/Client/webpack.config.js
@@ -10,12 +10,13 @@ module.exports = {
     devServer: {
         static: "./public",
         port: 8081,
-        proxy: {
-            '/Hearts/IHeartsApi/**': {
+        proxy: [
+            {
+                context: ['/Hearts/IHeartsApi/**'],
                 target: "http://127.0.0.1:5000/",
                 changeOrigin: true
             }
-        }
+        ]
     },
     resolve: {
         alias: {
