b9f3f61a2fcddb3d53e1cabd2bdb9dfab1002181
Brian Berns <brianberns@gmail.com>
2026-01-22 09:47:25 -0500

    Play model vs. heurisitc.

---
+++ Hearts.PlayModel/Program.fs
@@ -0,0 +1,30 @@
+﻿namespace Hearts.PlayKiller
+
+open System.Runtime
+
+open Hearts
+open Hearts.Heuristic
+open Hearts.Model
+
+module Program =
+
+    printfn $"Server garbage collection: {GCSettings.IsServerGC}"
+
+    let numDeals = 20_000
+
+    let player =
+        let model =
+            let model =
+                new AdvantageModel(
+                    hiddenSize = Encoding.encodedLength * 3,
+                    numHiddenLayers = 9,
+                    dropoutRate = 0.3,
+                    device = TorchSharp.torch.CUDA)
+            model.load("AdvantageModel.pt") |> ignore
+            model.eval()
+            model
+        Strategy.createPlayer model
+
+    let payoff =
+        Tournament.run 0 true numDeals Claude.player player
+    printfn $"Payoff: {payoff}"
