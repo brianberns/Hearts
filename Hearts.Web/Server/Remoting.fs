19b5af871fbc46bfab4e69392c812f8eb3e5a592
Brian Berns <brianberns@gmail.com>
2026-01-30 23:21:06 -0500

    Update hidden size to match latest model.

--- Hearts.Web/Server/Remoting.fs
+++ Hearts.Web/Server/Remoting.fs
@@ -18,7 +18,7 @@ module AdvantageModel =
     let private connect dir =
         let model =
             new AdvantageModel(
-                hiddenSize = Encoding.encodedLength * 2,
+                hiddenSize = Encoding.encodedLength * 3,
                 numHiddenLayers = 9,
                 dropoutRate = 0.3,
                 device = TorchSharp.torch.CPU)
