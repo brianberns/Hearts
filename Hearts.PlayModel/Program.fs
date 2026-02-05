7255a8e97548970acd545721497613fd69620a88
Brian Berns <brianberns@gmail.com>
2026-02-05 10:45:40 -0500

    Update hidden size.

--- Hearts.PlayModel/Program.fs
+++ Hearts.PlayModel/Program.fs
@@ -18,7 +18,7 @@ module Program =
         use model =
             let model =
                 new AdvantageModel(
-                    hiddenSize = Encoding.encodedLength * 2,
+                    hiddenSize = Encoding.encodedLength * 3,
                     numHiddenLayers = 9,
                     dropoutRate = 0.3,
                     device = TorchSharp.torch.CUDA)
