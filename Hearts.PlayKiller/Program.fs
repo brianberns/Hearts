fea62c909d3b1586e5302d10165e2f1641c1b868
Brian Berns <brianberns@gmail.com>
2026-02-17 22:38:44 -0500

    Updated to match local model.

--- Hearts.PlayKiller/Program.fs
+++ Hearts.PlayKiller/Program.fs
@@ -7,9 +7,9 @@ module Program =
     let model =
         let model =
             new AdvantageModel(
-                hiddenSize = Encoding.encodedLength * 3,
-                numHiddenLayers = 9,
-                dropoutRate = 0.3,
+                hiddenSize = Encoding.encodedLength * 2,
+                numHiddenLayers = 4,
+                dropoutRate = 0.0,
                 device = TorchSharp.torch.CPU)
         model.load("AdvantageModel.pt") |> ignore
         model.eval()
