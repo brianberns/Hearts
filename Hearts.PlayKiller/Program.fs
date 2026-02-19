e67b7cdc569f2125401c0f0c934f8eca2cab7686
Brian Berns <brianberns@gmail.com>
2026-02-19 14:32:17 -0500

    Play KHearts against a specific model file.

--- Hearts.PlayKiller/Program.fs
+++ Hearts.PlayKiller/Program.fs
@@ -1,22 +1,47 @@
 ﻿namespace Hearts.PlayKiller
 
+open System
+open System.Runtime
+open System.Text
+
+open Hearts.Learn
 open Hearts.Model
+open Hearts.Train
 
 module Program =
 
-    let model =
-        let model =
+    let parse (argv : _[]) =
+        if argv.Length = 0 then
+            "AdvantageModel.pt"
+        else
+            argv[0]
+
+    let run path =
+
+        let settings =
+            let writer = TensorBoard.createWriter ()
+            Settings.create writer
+        Settings.write settings
+
+        printfn $"Server garbage collection: {GCSettings.IsServerGC}"
+
+        use model =
             new AdvantageModel(
-                hiddenSize = Encoding.encodedLength * 2,
-                numHiddenLayers = 4,
-                dropoutRate = 0.0,
-                device = TorchSharp.torch.CPU)
-        model.load("AdvantageModel.pt") |> ignore
+                settings.HiddenSize,
+                settings.NumHiddenLayers,
+                0.0,
+                TorchSharp.torch.CPU)   // always run on CPU
+        model.load(path : string) |> ignore
         model.eval()
-        model
 
-    let player = Strategy.createPlayer model
+        let player = Strategy.createPlayer model
 
-    let payoffMap = Killer.run player
-    for (KeyValue(seat, payoff)) in payoffMap do
-        printfn "%A: %g" seat payoff
+        let payoffMap = Killer.run player
+        for (KeyValue(seat, payoff)) in payoffMap do
+            printfn "%A: %g" seat payoff
+
+    [<EntryPoint>]
+    let main argv =
+        Console.OutputEncoding <- Encoding.UTF8
+        parse argv |> run
+        0
